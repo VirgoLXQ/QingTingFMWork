2138e800b082d9467456d630d593b0bd5e1fb07b
ExcellentAndRomantic <2416677216@qq.com>
2020-07-13 12:26:17 +0800

    底部导航栏

--- app/src/main/java/com/lxqhmlwyh/qingtingfm/activity/MainActivity.java
+++ app/src/main/java/com/lxqhmlwyh/qingtingfm/activity/MainActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.lxqhmlwyh.qingtingfm.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+    private void initView(){
+        bottomNav=findViewById(R.id.main_bottom_nav);
     }
 }
